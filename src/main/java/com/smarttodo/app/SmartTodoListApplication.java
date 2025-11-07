fbf3fa8804beab29bd5e6d922ecd9aa817b99e2c
WhyTony <whytonycry@gmail.com>
2025-11-07 19:38:21 +0300

    init Spring Project

---
+++ src/main/java/com/smarttodo/app/SmartTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.smarttodo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarttodo/app/SmartTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttodo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
